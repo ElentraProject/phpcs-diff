4dfb4e6c8a742ff698a52dc8f9054caba542be78
Biplav <Biplav.Subedi@umanitoba.ca>
2024-04-22 20:36:28 -0500

    Fixed :: PHP 8 compat and default ruleset name

--- src/PhpcsDiff.php
+++ src/PhpcsDiff.php
@@ -187,7 +187,7 @@ class PhpcsDiff
      * @param string $ruleset
      * @return mixed
      */
-    protected function runPhpcs(array $files = [], string $ruleset = 'ruleset.xml')
+    protected function runPhpcs(array $files = [], string $ruleset = '.phpcs.xml.dist')
     {
         $exec = null;
         $root = dirname(__DIR__);
@@ -258,7 +258,7 @@ class PhpcsDiff
         );
 
         // Convert files into an array
-        $output = explode(PHP_EOL, $output);
+        $output = explode(PHP_EOL, (string)$output);
 
         // Remove any empty values
         return array_filter($output);
